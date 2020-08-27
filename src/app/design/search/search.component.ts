b1eff61609bd9234973b96d8519e7c000f511143
HyopeR <tol_celik@hotmail.com>
2020-08-27 23:41:05 +0300

    Html Pages arrangement

--- src/app/design/search/search.component.ts
+++ src/app/design/search/search.component.ts
@@ -1,9 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import {Router} from '@angular/router';
+import {Observable} from 'rxjs';
+import {isEmpty} from 'lodash';
+
+import {ActivatedRoute, Router} from '@angular/router';
 
 import { Region } from '../../model/region.model';
 import {RegionRepository} from '../../model/region.repository';
+import {map, take} from 'rxjs/operators';
+import {logger} from 'codelyzer/util/logger';
+import {SummonerRepository} from '../../model/summoner.repository';
 
 @Component({
   // tslint:disable-next-line:component-selector
@@ -17,14 +23,16 @@ export class SearchComponent implements OnInit {
   allRegionTag: string[];
   baseRegion: Region;
 
+
   summonerName: string = '';
 
   dataComplete: boolean = false;
 
   constructor(
     private regionRepository: RegionRepository,
+    private summonerRepository: SummonerRepository,
     private router: Router
-  ) {}
+  ) {  }
 
   ngOnInit() {
     this.baseRegion = this.regionRepository.serveBaseRegion();
